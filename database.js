31ab401fbb789b4441fac73ea3f46398d4e91d9a
duong-van-hai <91107289+duong-van-hai@users.noreply.github.com>
2026-01-09 23:56:51 +0700

    Update and rename data.js to database.js

---
+++ database.js
@@ -0,0 +1,15 @@
+export const db_lietsi = [
+    { id: "0001", name: "Võ Thị Sáu", type: "liệt sĩ", birth: "1933", death: "1952", img: "sau.jpg" },
+    { id: "0002", name: "Nguyễn Văn Trỗi", type: "liệt sĩ", birth: "1940", death: "1964", img: "troi.jpg" },
+    // Thêm đến 1000 người ở đây
+];
+
+export const db_quequan = {
+    "0001": { p: "Bà Rịa - Vũng Tàu", d: "Đất Đỏ", c: "Phước Long Thọ" },
+    "0002": { p: "Quảng Nam", d: "Điện Bàn", c: "Thanh Quýt" },
+};
+
+export const db_diachi = {
+    "0001": { r: "Nghĩa trang Hàng Dương", ct: "Việt Nam", bio: "Nữ anh hùng huyền thoại..." },
+    "0002": { r: "Nghĩa trang liệt sĩ TP.HCM", ct: "Việt Nam", bio: "Anh hùng bất khuất..." },
+};
